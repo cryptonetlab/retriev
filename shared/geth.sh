906328b00c0e64cbdaf65ba3d7f5f6899612c055
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-05-31 04:14:48 +0200

    adding local geth instance

---
+++ shared/geth.sh
@@ -0,0 +1,26 @@
+# Install a local Ethereum Node
+
+apt-get install software-properties-common
+add-apt-repository -y ppa:ethereum/ethereum
+apt-get update
+apt-get install ethereum
+
+echo "[Unit]
+Description=Geth
+
+[Service]
+Type=simple
+User=root
+Restart=always
+RestartSec=12
+ExecStart=/bin/geth --rinkeby --http
+
+[Install]
+WantedBy=default.target" > /etc/systemd/system/geth.service
+
+systemctl daemon-reload
+systemctl enable geth.service
+systemctl daemon-reload
+
+systemctl start geth
+systemctl status geth
