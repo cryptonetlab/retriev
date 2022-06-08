a58997a961141aabd97303e0b3c2a586b6ecf735
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-06-08 12:48:04 +0200

    fix folders

--- smart-contract/contracts/DataRetrievability.sol
+++ smart-contract/contracts/DataRetrievability.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./libs/render/IRENDER.sol";
+import "./functions/render/IRENDER.sol";
 
 /**
  * @title DataRetrievability
