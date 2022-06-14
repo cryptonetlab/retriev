45f3155bc61354bc6968d9285bb4fbdef96cc8b8
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-06-14 11:07:08 +0200

    fixing foundry tests

--- smart-contract/foundry_tests/test/Retriev.t.sol
+++ smart-contract/foundry_tests/test/Retriev.t.sol
@@ -14,30 +14,34 @@ contract RetrievTest is Test {
 
     // SETUP NETWORK
     function testAddProvider() public {
+        address provider = vm.addr(5);
         retriev.setProviderStatus(
-            0x2489c48090523f37028f1e42454b7DD04943d0Bf,
+            provider,
             true,
             "http://localhost:8000"
         );
         assertEq(
-            retriev.isProvider(0x2489c48090523f37028f1e42454b7DD04943d0Bf),
+            retriev.isProvider(provider),
             true
         );
     }
 
     function testAddReferees() public {
+        address referee1 = vm.addr(6);
+        address referee2 = vm.addr(7);
+        address referee3 = vm.addr(8);
         retriev.setRefereeStatus(
-            0x9E613de85626b5f0f261C14Cb715F54c4C37D6C6,
+            referee1,
             true,
             "http://localhost:7000"
         );
         retriev.setRefereeStatus(
-            0x6c3B6e30b19275B68C83cC963951883AB7B2290A,
+            referee2,
             true,
             "http://localhost:7001"
         );
         retriev.setRefereeStatus(
-            0x333766a11BC9A83A2bfa5a0A076a91ECb61B9a11,
+            referee3,
             true,
             "http://localhost:7002"
         );
@@ -45,19 +49,20 @@ contract RetrievTest is Test {
 
     // CREATE DEAL PROPOSAL
     function testCreateDealProposal() public {
+        address provider = vm.addr(5);
         retriev.setProviderStatus(
-            0x2489c48090523f37028f1e42454b7DD04943d0Bf,
+            provider,
             true,
             "http://localhost:8000"
         );
         assertEq(
-            retriev.isProvider(0x2489c48090523f37028f1e42454b7DD04943d0Bf),
+            retriev.isProvider(provider),
             true
         );
         uint256 duration = retriev.min_duration();
         uint256 collateral = 1 wei;
         address[] memory providers = new address[](1);
-        providers[0] = 0x2489c48090523f37028f1e42454b7DD04943d0Bf;
+        providers[0] = provider;
         retriev.createDealProposal{value: 1 wei}(
             "ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG",
             duration,
