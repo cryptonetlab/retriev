83cc952a5466d144261bfbc2dc14ab51d012a2e2
Steve Rogers <steve.rog02@gmail.com>
2022-06-16 09:07:42 +0200

    Navbar UI improvements

---
+++ clients-ui/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- Nav Button show/hide -->
+    <div
+      @click="
+        showNavbar = !showNavbar;
+        showConsole = false;
+      "
+      class="btn-sidebar position-top-right"
+    >
+      <i v-if="!showNavbar" class="fa-solid fa-bars"></i>
+      <i v-if="showNavbar" class="fa-solid fa-times"></i>
+    </div>
+    <!-- END - Logs button show/hide -->
+    <!-- Navbar -->
+    <Transition
+      enter-active-class="slide-in-right"
+      leave-active-class="slide-out-right"
+    >
+      <div v-if="showNavbar" class="right-col">
+        <div class="nav-container mt-5 pt-5">
+          <a href="/#/signup">
+            <button class="button is-rounded is-dark">
+              Signup Protocol
+            </button></a
+          >
+          <div class="mt-3">
+            <a href="/help"
+              ><i class="fa-solid fa-circle-question mr-2"></i>
+              <span>Help</span></a
+            >
+          </div>
+        </div>
+      </div>
+    </Transition>
+    <!-- END - Navbar -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      showNavbar: false,
+    };
+  },
+};
+</script>
--- clients-ui/src/router/index.js
+++ clients-ui/src/router/index.js
@@ -1,19 +1,25 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/signup",
+    name: "Signups",
+    component: Signup,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
