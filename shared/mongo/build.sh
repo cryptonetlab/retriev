394b6dc006b410ea54471304098cfe63f4b1a942
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-07-07 03:36:38 +0200

    fix address binding

--- shared/mongo/build.sh
+++ shared/mongo/build.sh
@@ -24,7 +24,7 @@ if [[ "$@" =~ "-rebuild" ]]
 then
     echo "Rebuilding Docker Image"
     docker build --no-cache -t db:mongo .
-    docker run --restart=unless-stopped -d --name=mongodb -dit -p 27017:27017 db:mongo
+    docker run --restart=unless-stopped -d --name=mongodb -dit -p 127.0.0.1:27017:27017 db:mongo
 else
     docker ps | grep 'mongodb' &> /dev/null
     if [ $? == 0 ]; then
@@ -36,6 +36,6 @@ else
         echo 'DOCKER_OPTS="--iptables=false"' >> /etc/default/docker
         systemctl restart docker
         docker build -t db:mongo .
-        docker run --restart=unless-stopped -d --name=mongodb -dit -p 27017:27017 db:mongo
+        docker run --restart=unless-stopped -d --name=mongodb -dit -p 127.0.0.1:27017:27017 db:mongo
     fi
 fi
