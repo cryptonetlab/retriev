a80cc49fe5be74ca9cf935f7c2bd72b5f29e4765
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-09-30 11:48:00 +0200

    min fix

--- api/src/libs/web3.ts
+++ api/src/libs/web3.ts
@@ -131,7 +131,7 @@ export const parseDeal = async (deal_index, proposal_tx = '', accept_tx = '', ca
         if (cancel_tx === '') {
           cancel_tx = checkDB.cancel_tx
         }
-        await db.update('deals', { index: deal_index }, { $set: { canceled: deal.canceled, timestamp_start: deal.timestamp_start, timestamp_end: deal.timestamp_end, provider: provider, appeal_requested: deal.appeal_requested, accept_tx: accept_tx, cancel_tx: cancel_tx } })
+        await db.update('deals', { index: deal_index, contract: process.env.CONTRACT_ADDRESS }, { $set: { canceled: deal.canceled, timestamp_start: deal.timestamp_start, timestamp_end: deal.timestamp_end, provider: provider, appeal_requested: deal.appeal_requested, accept_tx: accept_tx, cancel_tx: cancel_tx } })
       }
       response(true)
     } else {
